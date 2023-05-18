97dac3e008cb2a4b645a9c4b2569a7a5eb61bf93
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-05-18 21:05:51 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ fetch(
     response.results.forEach(movie => {
       const movieDiv = document.createElement('div');
       movieDiv.innerHTML = `
-        <h2>${movie.title || movie.name}</h2>
-        <p>${movie.overview}</p>
+       
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
         movie.title || movie.name
       } Poster">
+       <h2>${movie.title || movie.name}</h2>
+        <p>${movie.overview}</p>
       `;
       resultDiv.appendChild(movieDiv);
     });
