b0ab4627f38fd08352aadff3f14dee2082dc57fe
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-05-26 06:17:13 +0200

    fix pag
    fix pag

--- src/index.js
+++ src/index.js
@@ -60,14 +60,14 @@ function createPagination() {
 
     const paginationList = document.createElement('ul');
     paginationList.classList.add('pagination__list');
-
     const prevButton = document.createElement('li');
     prevButton.classList.add('pagination__item', 'pagination__item-button');
-    const prevButtonIcon = document.createElement('img');
+    const prevButtonIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     prevButtonIcon.classList.add('pagination__icon-arrow', 'pagination__icon-arrow--left');
-    prevButtonIcon.setAttribute('src', './images/arrow-left.svg');
-    prevButtonIcon.setAttribute('width', '16');
-    prevButtonIcon.setAttribute('height', '16');
+    prevButtonIcon.setAttribute('width', '16px');
+    prevButtonIcon.setAttribute('height', '16px');
+    prevButtonIcon.setAttribute('viewBox', '0 0 16 16');
+    prevButtonIcon.innerHTML = `<path d="M6.293 13.707l-5-5c-0.391-0.39-0.391-1.024 0-1.414l5-5c0.391-0.391 1.024-0.391 1.414 0s0.391 1.024 0 1.414l-3.293 3.293h9.586c0.552 0 1 0.448 1 1s-0.448 1-1 1h-9.586l3.293 3.293c0.195 0.195 0.293 0.451 0.293 0.707s-0.098 0.512-0.293 0.707c-0.391 0.391-1.024 0.391-1.414 0z"></path>`;
     prevButton.appendChild(prevButtonIcon);
     paginationList.appendChild(prevButton);
 
@@ -87,11 +87,12 @@ function createPagination() {
 
     const nextButton = document.createElement('li');
     nextButton.classList.add('pagination__item', 'pagination__item-button');
-    const nextButtonIcon = document.createElement('img');
+    const nextButtonIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     nextButtonIcon.classList.add('pagination__icon-arrow', 'pagination__icon-arrow--right');
-    nextButtonIcon.setAttribute('src', './images/arrow-right.svg');
-    nextButtonIcon.setAttribute('width', '16');
-    nextButtonIcon.setAttribute('height', '16');
+    nextButtonIcon.setAttribute('width', '16px');
+    nextButtonIcon.setAttribute('height', '16px');
+    nextButtonIcon.setAttribute('viewBox', '0 0 16 16');
+    nextButtonIcon.innerHTML = `<path d="M9.707 13.707l5-5c0.391-0.39 0.391-1.024 0-1.414l-5-5c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414l3.293 3.293h-9.586c-0.552 0-1 0.448-1 1s0.448 1 1 1h9.586l-3.293 3.293c-0.195 0.195-0.293 0.451-0.293 0.707s0.098 0.512 0.293 0.707c0.391 0.391 1.024 0.391 1.414 0z"></path>`;
     nextButton.appendChild(nextButtonIcon);
     paginationList.appendChild(nextButton);
 
@@ -106,13 +107,13 @@ function createPagination() {
           resultDiv.innerHTML = '';
           FavoritesMovies();
         }
-      } else if (target === prevButton) {
+      } else if (target === prevButton || target === prevButtonIcon) {
         if (currentPage > 1) {
           currentPage--;
           resultDiv.innerHTML = '';
           FavoritesMovies();
         }
-      } else if (target === nextButton) {
+      } else if (target === nextButton || target === nextButtonIcon) {
         if (currentPage < totalPages) {
           currentPage++;
           resultDiv.innerHTML = '';
@@ -178,11 +179,84 @@ function searchMovies(event) {
             `;
             searchMovieDiv.appendChild(movieDiv);
           });
-        })
-        .catch(err => console.error(err));
+          totalPages = response.total_pages;
+          createPagination();
+        });
     })
     .catch(err => console.error(err));
 }
+function createPagination() {
+  const paginationContainer = document.querySelector('.pagination');
+  if (paginationContainer) {
+    paginationContainer.innerHTML = '';
+
+    const paginationList = document.createElement('ul');
+    paginationList.classList.add('pagination__list');
+    const prevButton = document.createElement('li');
+    prevButton.classList.add('pagination__item', 'pagination__item-button');
+    const prevButtonIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    prevButtonIcon.classList.add('pagination__icon-arrow', 'pagination__icon-arrow--left');
+    prevButtonIcon.setAttribute('width', '16px');
+    prevButtonIcon.setAttribute('height', '16px');
+    prevButtonIcon.setAttribute('viewBox', '0 0 16 16');
+    prevButtonIcon.innerHTML = `<path d="M6.293 13.707l-5-5c-0.391-0.39-0.391-1.024 0-1.414l5-5c0.391-0.391 1.024-0.391 1.414 0s0.391 1.024 0 1.414l-3.293 3.293h9.586c0.552 0 1 0.448 1 1s-0.448 1-1 1h-9.586l3.293 3.293c0.195 0.195 0.293 0.451 0.293 0.707s-0.098 0.512-0.293 0.707c-0.391 0.391-1.024 0.391-1.414 0z"></path>`;
+    prevButton.appendChild(prevButtonIcon);
+    paginationList.appendChild(prevButton);
+
+    const startPage = Math.max(currentPage - 2, 1);
+    const endPage = Math.min(startPage + 4, totalPages);
+    for (let i = startPage; i <= endPage; i++) {
+      const pageButton = document.createElement('li');
+      pageButton.classList.add('pagination__item');
+      if (i === currentPage) {
+        pageButton.classList.add('active');
+      }
+      const pageSpan = document.createElement('span');
+      pageSpan.textContent = i.toString();
+      pageButton.appendChild(pageSpan);
+      paginationList.appendChild(pageButton);
+    }
+
+    const nextButton = document.createElement('li');
+    nextButton.classList.add('pagination__item', 'pagination__item-button');
+    const nextButtonIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    nextButtonIcon.classList.add('pagination__icon-arrow', 'pagination__icon-arrow--right');
+    nextButtonIcon.setAttribute('width', '16px');
+    nextButtonIcon.setAttribute('height', '16px');
+    nextButtonIcon.setAttribute('viewBox', '0 0 16 16');
+    nextButtonIcon.innerHTML = `<path d="M9.707 13.707l5-5c0.391-0.39 0.391-1.024 0-1.414l-5-5c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414l3.293 3.293h-9.586c-0.552 0-1 0.448-1 1s0.448 1 1 1h9.586l-3.293 3.293c-0.195 0.195-0.293 0.451-0.293 0.707s0.098 0.512 0.293 0.707c0.391 0.391 1.024 0.391 1.414 0z"></path>`;
+    nextButton.appendChild(nextButtonIcon);
+    paginationList.appendChild(nextButton);
+
+    paginationContainer.appendChild(paginationList);
+
+    paginationList.addEventListener('click', event => {
+      const target = event.target;
+      if (target.tagName === 'SPAN') {
+        const pageIndex = parseInt(target.textContent);
+        if (pageIndex >= 1 && pageIndex <= totalPages) {
+          currentPage = pageIndex;
+          resultDiv.innerHTML = '';
+          FavoritesMovies();
+        }
+      } else if (target === prevButton || target === prevButtonIcon) {
+        if (currentPage > 1) {
+          currentPage--;
+          resultDiv.innerHTML = '';
+          FavoritesMovies();
+        }
+      } else if (target === nextButton || target === nextButtonIcon) {
+        if (currentPage < totalPages) {
+          currentPage++;
+          resultDiv.innerHTML = '';
+          FavoritesMovies();
+        }
+      }
+    });
+  }
+}
+
+createPagination();
 
 // Wywołanie funkcji searchMovies() po kliknięciu przycisku "Szukaj"
 document.getElementById('search-button').addEventListener('click', searchMovies);
